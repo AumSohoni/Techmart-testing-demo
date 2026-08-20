23edc700b2bf2b488a949dc473f9ea0b32359034
Aum <163599665+AumSohoni@users.noreply.github.com>
2026-08-20 13:23:55 +0300

    Update timeout format in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,6 +47,6 @@ export default defineConfig({
         command: 'npm start --prefix sample-app',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
-        timeout: 120 * 1000,
+        timeout: 120_000,
     },
 });
